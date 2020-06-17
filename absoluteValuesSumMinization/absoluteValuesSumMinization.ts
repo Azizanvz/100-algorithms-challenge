f709b7ed94e047ddfbd2fb9e0425c4b6d948e34e
Aziza Novruzova <Azizanvz@github.com>
2020-06-16 20:39:51 -0400

    The challenge was to simply find median value

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,11 @@
-function absoluteValuesSumMinimization(a: number[]): number {
+function absoluteValuesSumMinimization(a: number[]): number { // median number
 
+    const isEven = a.length % 2 === 0; // first, we need to check if number of elements in the arrays is even
+
+    return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)]; // if even we take one on the left as median, else the one in the middle
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
