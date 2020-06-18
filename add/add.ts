8f53adec34d70903356661894885745eb47c2ae8
Aziza Novruzova <Azizanvz@github.com>
2020-06-18 07:03:50 -0400

    its rest parameter, not spread operator

--- add/add.ts
+++ add/add.ts
@@ -2,7 +2,7 @@ function add(param1: number, param2: number): number {
     return param1 + param2;
 }
 
-function add2(...param1: number[]): number { //typescript allows us to declare arguments type, and we use spread operator
+function add2(...param1: number[]): number { //typescript allows us to declare arguments type, and we use rest parameter
 
     let sum = 0;
     param1.forEach(num => { // we simply go through the array and sum the values
